c5798c4b979bc2279857796ad7733378c974c874
TANZAME <tian_naixiang@sina.com>
2021-05-27 16:25:32 +0800

    Update 查询耗CPU的sql语句.sql

--- 脚本/性能/查询耗CPU的sql语句.sql
+++ 脚本/性能/查询耗CPU的sql语句.sql
@@ -51,3 +51,62 @@ FROM sys.dm_exec_query_stats qs WITH(nolock)
 CROSS apply sys.dm_exec_sql_text(qs.sql_handle) AS qt
 WHERE execution_count>1
 ORDER BY  qs.total_worker_time/qs.execution_count DESC
+
+
+
+SELECT TOP 10
+   total_worker_time/execution_count AS avg_cpu_cost, plan_handle,
+   execution_count,
+   (SELECT SUBSTRING(text, statement_start_offset/2 + 1,
+      (CASE WHEN statement_end_offset = -1
+         THEN LEN(CONVERT(nvarchar(max), text)) * 2
+         ELSE statement_end_offset
+      END - statement_start_offset)/2)
+   FROM sys.dm_exec_sql_text(sql_handle)) AS query_text
+FROM sys.dm_exec_query_stats
+ORDER BY [avg_cpu_cost] DESC
+
+
+
+-- 按照物理读的页面数排序 前50名
+SELECT TOP 50
+ qs.total_physical_reads, -- 计划自编译后在执行期间所执行的物理读取总次数
+ qs.execution_count,	  -- 计划自上次编译以来所执行的次数	
+ qs.total_physical_reads/qs.execution_count AS [avg I/O], -- 平均读取的物理次数(页数)
+ qs. creation_time,		  -- 编译计划的时间
+ qs.max_elapsed_time,
+ qs.min_elapsed_time,
+ SUBSTRING(qt.text,qs.statement_start_offset/2,
+ (CASE WHEN qs.statement_end_offset=-1
+ THEN LEN(CONVERT(NVARCHAR(max),qt.text))*2
+ ELSE qs.statement_end_offset END -qs.statement_start_offset)/2) AS query_text,
+ qt.dbid,dbname=DB_NAME(qt.dbid),
+ qt.objectid,
+ qs.sql_handle,
+ qs.plan_handle
+ from sys.dm_exec_query_stats qs
+ CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+ ORDER BY qs.total_physical_reads/qs.execution_count DESC
+ 
+ -- 按照逻辑读的页面数排序 前50名
+ SELECT TOP 50
+ qs.total_logical_reads,
+ qs.execution_count,
+  qs.max_elapsed_time,
+ qs.min_elapsed_time,
+ qs.total_logical_reads/qs.execution_count AS [AVG IO],
+ SUBSTRING(qt.text,qs.statement_start_offset/2,
+ (CASE WHEN qs.statement_end_offset=-1 
+ THEN LEN(CONVERT(NVARCHAR(max),qt.text)) *2
+  ELSE qs.statement_end_offset END -qs.statement_start_offset)/2) 
+  AS query_text,
+ qt.dbid,
+ dbname=DB_NAME(qt.dbid),
+ qt.objectid,
+ qs.sql_handle,
+  creation_time,
+ qs.plan_handle
+ from sys.dm_exec_query_stats qs
+ CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) AS qt
+ ORDER BY qs.total_logical_reads/qs.execution_count DESC
+ 
